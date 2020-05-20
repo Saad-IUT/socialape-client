f5c9ef731b16b743d84c6d904aca0f879abd562f
Saad <saadbinjohir@gmail.com>
2020-05-20 20:04:07 +0600

    Post Card Details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
 //Components
 import NavBar from './components/NavBar'
@@ -10,10 +13,31 @@ import home from './pages/home'
 import login from './pages/login'
 import signup from './pages/signup'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+})
+
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <MuiThemeProvider theme={theme}>
+        {/* <Provider store={store}> */}
         <Router>
           <NavBar />
           <div className='container'>
@@ -21,10 +45,17 @@ class App extends Component {
               <Route exact path='/' component={home} />
               <Route exact path='/login' component={login} />
               <Route exact path='/signup' component={signup} />
+              {/* <Route exact path='/users/:handle' component={user} /> */}
+              {/* <Route
+                exact
+                path='/users/:handle/scream/:screamId'
+                component={user}
+              /> */}
             </Switch>
           </div>
         </Router>
-      </div>
+        {/* </Provider> */}
+      </MuiThemeProvider>
     )
   }
 }
