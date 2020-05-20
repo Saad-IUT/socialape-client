5eea99af662637e88bf0e309ebdb987054c92b06
Saad <saadbinjohir@gmail.com>
2020-05-20 23:19:25 +0600

    Login Form

--- src/components/Scream.jsx
+++ src/components/Scream.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 // MUI Stuff
 import Card from '@material-ui/core/Card';
@@ -25,6 +27,7 @@ const styles = {
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime)
     const {
       classes,
       scream: {
@@ -50,7 +53,7 @@ class Scream extends Component {
         <CardContent className={classes.content}>
           <Typography variant="h5" component={Link}
             to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
-          <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+          <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
       </Card>
