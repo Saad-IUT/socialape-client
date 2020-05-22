77ffdef90827ab92b859cc9938f6e020057d6c84
Saad <saadbinjohir@gmail.com>
2020-05-22 16:04:31 +0600

    Displaying Comments

--- src/components/scream/ScreamDialog.jsx
+++ src/components/scream/ScreamDialog.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import MyButton from '../../util/MyButton';
 import LikeButton from './LikeButton';
-// import Comments from './Comments';
+import Comments from './Comments';
 // import CommentForm from './CommentForm';
 import dayjs from 'dayjs';
 import { Link } from 'react-router-dom';
@@ -88,7 +88,7 @@ class ScreamDialog extends Component {
         commentCount,
         userImage,
         userHandle,
-        // comments
+        comments
       },
       UI: { loading }
     } = this.props;
@@ -126,7 +126,7 @@ class ScreamDialog extends Component {
           </Grid>
           <hr className={classes.visibleSeparator} />
           {/* <CommentForm screamId={screamId} /> */}
-          {/* <Comments comments={comments} /> */}
+          <Comments comments={comments} />
         </Grid>
       );
     return (
